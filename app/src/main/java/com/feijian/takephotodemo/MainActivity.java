41e1289c391b115ad3110331a2544638e91794c4
Giousa <65489469@qq.com>
2018-06-08 15:27:13 +0800

    7.0权限

--- app/src/main/java/com/feijian/takephotodemo/MainActivity.java
+++ app/src/main/java/com/feijian/takephotodemo/MainActivity.java
@@ -1,11 +1,17 @@
 package com.feijian.takephotodemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import java.io.File;
 
 
 public class MainActivity extends Activity implements InitTimetoTakePic.OnTakePicListener {
@@ -21,6 +27,23 @@ public class MainActivity extends Activity implements InitTimetoTakePic.OnTakePi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        File file=new File(Environment.getExternalStorageDirectory(), "/a_picture/pic/"+System.currentTimeMillis() + ".jpg");
+        if (!file.getParentFile().exists())file.getParentFile().mkdirs();
+        Uri imageUri = Uri.fromFile(file);
+        Intent intent = new Intent();
+        //设置Action为拍照
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        //将拍取的照片保存到指定URI
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent,1);
+
+
+
         // 创建预览类，并与Camera关联，最后添加到界面布局中
 
         fl_preview = (FrameLayout) findViewById(R.id.camera_preview);
